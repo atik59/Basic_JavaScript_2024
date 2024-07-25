fab9a6992c5366150e6848e612619a482ae63da3
Atikur Rahman <104792832+atik59@users.noreply.github.com>
2024-07-26 00:19:50 +0600

    Add files via upload

---
+++ Class 01 (Node js install and code run)/javaScript.js
@@ -0,0 +1,11 @@
+console.log("ATik");
+
+var name = "ATik";
+var roll = 1059;
+
+console.log(name, roll);
+
+
+console.log("My name is",name,"And my Class roll :",roll);
+
+console.log("My name is "+name+" And my Class roll : "+roll);
