005b028ee84be9168e775c6cc819419b33b630b0
Atikur Rahman <104792832+atik59@users.noreply.github.com>
2024-07-28 00:06:29 +0600

    Add files via upload

---
+++ Class 02 (Variable)/javaScript.js
@@ -0,0 +1,34 @@
+console.log("ATik");
+
+
+// valid
+
+var name = "ATik";
+var number = 77;
+var variableName = "value";
+var first_Name = "ATik";
+var number01 = 12;
+
+console.log(name);
+console.log(number);
+console.log(number01);
+console.log(first_Name);
+console.log(variableName);
+
+
+//invalid
+
+// var if = "abc";
+// var for = "abc";
+// var 12 = "ATik;
+// var 12Name = "Atik";
+// var first-Name = "ATIk";
+// var first@Name = "ATik";
+
+
+// console.log(if);
+// console.log(for);
+console.log(12)
+// console.log(12Name);
+// console.log(first-Name);
+// console(first@Name);
