015a14c1ba9e9ad43c28b6ae0931af03cc5ed162
Atikur Rahman <104792832+atik59@users.noreply.github.com>
2024-07-29 02:38:15 +0600

    Add files via upload

---
+++ Class 03 (Mathematical Operators)/javaScript.js
@@ -0,0 +1,51 @@
+console.log("ATik");
+console.log(10+30);
+
+var num01 = 20;
+var num02 = 30;
+console.log(num01+num02);
+
+var result = num01 + num02;
+console.log(result);
+
+console.log(num01 + 10);
+
+
+var sum = num01 + num02;
+var sub =  num01 - num02;
+var mul = num01 * num02;
+var divi = num01 / num02
+var remainder =  num01 % num02;
+
+console.log(sum, sub, mul, divi, remainder);
+
+
+var total = sum + sub + divi + mul + remainder;
+console.log(total);
+
+
+var braket = (num01 + ((sum +  divi) - (remainder* divi) + mul));
+console.log(braket);
+
+
+
+
+
+// check the Type
+// using function typeof();
+
+
+console.log(typeof(sum));
+console.log(typeof(sub));
+console.log(typeof(mul));
+console.log(typeof(divi));
+console.log(typeof(remainder));
+
+
+console.log(typeof("ATik"));
+console.log(typeof("10"));
+console.log(typeof(10));
+console.log(typeof(true));
+console.log(typeof(false));
+console.log(typeof("@"));
+
