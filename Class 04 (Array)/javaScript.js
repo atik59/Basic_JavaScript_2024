8fe7fb5cd306d0948586fb43fa4b140ba949d35f
Atikur Rahman <104792832+atik59@users.noreply.github.com>
2024-08-02 00:05:30 +0600

    Add files via upload

---
+++ Class 04 (Array)/javaScript.js
@@ -0,0 +1,35 @@
+console.log("ATik");
+
+var numbers = [10, 20, 30, 40, 50];
+console.log(numbers);
+console.log(numbers[0]);
+console.log(numbers[1]);
+console.log(numbers[2]);
+console.log(numbers[3]);
+console.log(numbers[4]);
+
+var names =  ["ATik", "Habib", "Yousuf", "Tamal", "Asif"];
+console.log(names);
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+console.log(names[4]);
+
+
+var mixsdata = [10,20,"ATik", "Habib",true, false];
+console.log(mixsdata);
+console.log(mixsdata[0]);
+console.log(mixsdata[1]);
+console.log(mixsdata[2]);
+console.log(mixsdata[3]);
+console.log(mixsdata[4]);
+console.log(mixsdata[5]);
+
+
+console.log(numbers.length);
+console.log(names.length);
+console.log(mixsdata.length);
+
+
+
