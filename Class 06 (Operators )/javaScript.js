02a2889e1c55b1d6ce7ea105190fe8b33927b4cc
Atikur Rahman <104792832+atik59@users.noreply.github.com>
2024-08-03 00:43:09 +0600

    Add files via upload

---
+++ Class 06 (Operators )/javaScript.js
@@ -0,0 +1,42 @@
+console.log("MD. Atikur Rahman");
+
+// ==
+// ===
+// >
+// <
+// >=
+// <=
+// !=
+// !==
+
+// &&
+// ||
+
+console.log(100 == 100);
+console.log(100 == 1000);
+
+console.log('10' == '10');
+console.log(10 == '10');
+console.log(10 === '10');
+console.log('10' === '10');
+console.log(10 > 20);
+console.log(10 < 20);
+console.log(10 >=10);
+console.log(10>=20);
+console.log(10<=20);
+console.log(10 != 10);
+console.log(10 != 20);
+console.log(10 !== '10');
+console.log('10' !== '10');
+
+console.log(20>10 && 20>10);
+console.log(20<10 && 20>10);
+console.log(20<10 && 20<10);
+console.log(20>10 || 20>10);
+console.log(20<10 || 20>10);
+console.log(20<10 || 20<10);
+
+
+console.log(!false);
+console.log(!true);
+
