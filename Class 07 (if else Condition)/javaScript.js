8d8415439552d570419a69be5476c35080848832
Atikur Rahman <104792832+atik59@users.noreply.github.com>
2024-08-05 02:25:52 +0600

    Add files via upload

---
+++ Class 07 (if else Condition)/javaScript.js
@@ -0,0 +1,34 @@
+console.log("MD. ATIKUR RAHMAN");
+
+console.log("ATIK" === "Atik");
+console.log("ATIK" == "atik");
+
+var num = 10;
+
+
+//IF condition
+if(num === 10)
+{
+    console.log("IF Condition True");
+}
+else
+{
+    console.log("ELSE Condition True");
+}
+
+
+// ELSE IF Structure 
+
+
+if(num === 10)
+{
+    console.log("IF Condition True");
+}
+else if(num === 20)
+{
+    console.log("ELSE-IF Condition True");
+}
+else
+{
+    console.log("ELSE Condition True");
+}
